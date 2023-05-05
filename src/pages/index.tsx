67fab9d30710ed9eaf8eccd1ecc24b92d8003e25
Norbert <katunanorbert@gmail.com>
2023-05-05 17:37:09 +0300

    added table component and config file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,48 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import Balance from '../components/Balance'
-import { Inter } from 'next/font/google'
+import Table from '@/components/Table'
 import styles from '@/styles/Home.module.css'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import Balance from '../components/Balance'
+import Header from '../components/Header'
+import config from '../metadata/config.json'
+
+console.log(config)
 
 const inter = Inter({ subsets: ['latin'] })
+
+const tableColumns = [
+  {
+    Header: 'Coin',
+    accessor: 'col1'
+  },
+  {
+    Header: 'Price',
+    accessor: 'col2'
+  },
+  {
+    Header: 'Amount',
+    accessor: 'col3'
+  },
+  {
+    Header: 'Next prediction',
+    accessor: 'col4'
+  },
+  {
+    Header: 'Current Prediction',
+    accessor: 'col5'
+  }
+]
+
+let tableData = []
+config.forEach((data) => {
+  let index = 1
+  let row = {}
+  for (const [key, value] of Object.entries(data)) {
+    row[`col${index}`] = value
+    index++
+  }
+  tableData.push(row)
+})
 
 export default function Home() {
   return (
@@ -19,6 +57,7 @@ export default function Home() {
         <Header />
         <h1>Predictoor</h1>
         <Balance />
+        <Table columns={tableColumns} data={tableData} />
       </main>
     </>
   )
