a2d9f4360e7b54ebfc51ebdedacc3aa8932f60f9
Norbert <katunanorbert@gmail.com>
2023-05-12 10:54:43 +0300

    add disabled prop to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,17 @@ import styles from '../styles/Button.module.css'
 
 interface ButtonProps {
   text: string
+  disabled?: boolean
   onClick: () => void
 }
 
-export default function Button({ text, onClick }: ButtonProps) {
+export default function Button({ text, disabled, onClick }: ButtonProps) {
   return (
-    <button onClick={() => onClick()} className={styles.button}>
+    <button
+      onClick={() => onClick()}
+      className={styles.button}
+      disabled={disabled}
+    >
       {text}
     </button>
   )
