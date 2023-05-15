19d1f3aeb8fb1630446c70bf8a7554a70280a684
Norbert <katunanorbert@gmail.com>
2023-05-15 14:13:39 +0300

    add chain config for sapphire testnet

---
+++ src/metadata/networksConfig.ts
@@ -0,0 +1,26 @@
+import { Chain } from 'wagmi'
+
+export const oasis = {
+  id: 23295,
+  name: 'Oasis Sapphire Testnet',
+  network: 'Oasis Sapphire Testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Oasis Network',
+    symbol: 'ROSE'
+  },
+  rpcUrls: {
+    public: { http: ['https://testnet.sapphire.oasis.dev'] },
+    default: { http: ['https://testnet.sapphire.oasis.devc'] }
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'SapphireTestnet',
+      url: 'https://testnet.explorer.sapphire.oasis.dev'
+    },
+    default: {
+      name: 'SapphireTestnet',
+      url: 'https://testnet.explorer.sapphire.oasis.dev'
+    }
+  }
+} as const satisfies Chain
