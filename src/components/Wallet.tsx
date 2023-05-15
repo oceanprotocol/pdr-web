fff703a6b44d3d1aad770685939180e3a96d7368
Norbert <katunanorbert@gmail.com>
2023-05-15 16:44:36 +0300

    add networks json and fetch network name from there

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,19 +1,32 @@
 import styles from '@/styles/Wallet.module.css'
+import { getNetworkName } from '@/utils/network'
 import { Web3Button } from '@web3modal/react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNetwork, useSwitchNetwork } from 'wagmi'
 import config from '../metadata/config.json'
 import Button from './Button'
 
 export default function Wallet() {
   const { chain } = useNetwork()
+  const [loading, setLoading] = useState<boolean>(true)
+  const [networkName, setNetworkName] = useState<string | undefined>(undefined)
   const { isLoading, pendingChainId, switchNetwork } = useSwitchNetwork()
+
+  const saveNetworkName = async () => {
+    if (!chain) return
+    let name = await getNetworkName(chain.id)
+    setNetworkName(name)
+  }
+
   useEffect(() => {
-    chain && console.log(chain)
+    if (chain) {
+      setLoading(false)
+      saveNetworkName()
+    }
   }, [chain])
   return (
     <div className={styles.container}>
-      {chain && parseInt(config[0].chainId) !== chain?.id && (
+      {!loading && chain && parseInt(config[0].chainId) !== chain?.id && (
         <Button
           disabled={!switchNetwork || parseInt(config[0].chainId) === chain?.id}
           onClick={() => switchNetwork?.(parseInt(config[0].chainId))}
@@ -25,7 +38,9 @@ export default function Wallet() {
         />
       )}
       <div className={styles.walletInfoContainer}>
-        {chain && <span className={styles.chainName}>{chain.name}</span>}
+        {!loading && chain && networkName && (
+          <span className={styles.chainName}>{networkName}</span>
+        )}
         <Web3Button />
       </div>
     </div>
