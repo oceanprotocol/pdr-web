8a906de6de8e7775ed003aedcdd75d442d1183c4
Norbert <katunanorbert@gmail.com>
2023-05-15 16:05:51 +0300

    update switch network and fix UI

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -3,30 +3,31 @@ import { Web3Button } from '@web3modal/react'
 import { useEffect } from 'react'
 import { useNetwork, useSwitchNetwork } from 'wagmi'
 import config from '../metadata/config.json'
+import Button from './Button'
 
 export default function Wallet() {
   const { chain } = useNetwork()
-  const { error, isLoading, pendingChainId, switchNetwork } = useSwitchNetwork()
+  const { isLoading, pendingChainId, switchNetwork } = useSwitchNetwork()
   useEffect(() => {
     chain && console.log(chain)
   }, [chain])
   return (
     <div className={styles.container}>
       {chain && parseInt(config[0].chainId) !== chain?.id && (
-        <button
+        <Button
           disabled={!switchNetwork || parseInt(config[0].chainId) === chain?.id}
-          key={parseInt(config[0].chainId)}
           onClick={() => switchNetwork?.(parseInt(config[0].chainId))}
-        >
-          Ethereum
-          {isLoading &&
-            pendingChainId === parseInt(config[0].chainId) &&
-            ' (switching)'}
-        </button>
+          text={
+            isLoading && pendingChainId === parseInt(config[0].chainId)
+              ? 'Switching to Ethereum...'
+              : 'Switch Network to Ethereum'
+          }
+        />
       )}
-      {chain && <span className={styles.chainName}>{chain.name}</span>}
-      <div>{error && error.message}</div>
-      <Web3Button />
+      <div className={styles.walletInfoContainer}>
+        {chain && <span className={styles.chainName}>{chain.name}</span>}
+        <Web3Button />
+      </div>
     </div>
   )
 }
