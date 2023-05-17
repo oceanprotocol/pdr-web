71517acb3c629f2799b9ad2f7b34e7d325c8280e
idiom-bytes <69865342+idiom-bytes@users.noreply.github.com>
2023-05-17 13:03:10 -0700

    route is now working, everything seems to be initializing properly, and results are being yielded correlty. Server errors, unauthorized, and other handlers were tested.

--- src/pages/api/admin/consume-feeds.ts
+++ src/pages/api/admin/consume-feeds.ts
@@ -1,4 +1,4 @@
-import { consumePredictoor } from '@/utils/predictoor';
+// import { consumePredictoor } from '@/utils/predictoor';
 import { ethers } from 'ethers';
 import { NextApiRequest, NextApiResponse } from 'next';
 import config from '../../../metadata/config.json';
@@ -7,12 +7,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     // Set appropriate CORS headers to allow external requests
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     // Check if the admin password is provided
-    const adminPassword = req.body.password || req.headers['x-admin-password'];
-  if (adminPassword !== process.env.NEXT_PUBLIC_ADMIN_PASSWORD ) {
+    const adminPassword = req.body.adminPassword || req.headers['x-admin-password'];
+    if (adminPassword !== process.env.NEXT_PUBLIC_ADMIN_PASSWORD ) {
       res.status(401).json({ error: 'Unauthorized' });
       return;
     }
@@ -25,23 +25,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const predictoorPK = process.env.NEXT_PUBLIC_PREDICTOOR_PK || ''
 
     // Create an Ethereum provider
-    const provider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_PREDICTOOR_RPC);
+    const provider = new ethers.providers.JsonRpcProvider(predictoorRPC);
        
     // infura provider
     const infuraProviderETH = new ethers.providers.InfuraProvider(
       'homestead',
       predictoorRPC
     )
-    
+
     const predictoorWallet = new ethers.Wallet(predictoorPK, infuraProviderETH)
 
     // Loop through each tokenPrediction object and call consumePredictoor
     for (const tokenPrediction of tokenPredictions) {
-      await consumePredictoor(tokenPrediction, predictoorWallet, provider);
+      // await consumePredictoor(tokenPrediction, predictoorWallet, provider);
     }
 
     // Send a JSON response
-    res.status(200).json({ message: 'Feeds consumed successfully' });
+    res.status(200).json({ 
+      message: 'Feeds consumed successfully', 
+      predictoorRPC: predictoorRPC, 
+      predictoorPK: predictoorPK,
+      provider: provider,
+      infuraProviderETH: infuraProviderETH,
+      predictoorWallet: predictoorWallet
+    });
+
   } catch (error) {
     // Handle any errors that occurred during the process
     console.error('Error consuming feeds:', error);
