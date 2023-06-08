5f062cc53555a4a81e14b91fae4f305fce2234b7
Mustafa Tuncay <mustafaislev@gmail.com>
2023-06-08 15:37:32 +0300

    issue40-fix for eslint

--- src/utils/appconstants.ts
+++ src/utils/appconstants.ts
@@ -4,8 +4,10 @@ export const currentConfig = process.env.NEXT_PUBLIC_ENV
   ? config[process.env.NEXT_PUBLIC_ENV as keyof typeof config]
   : config['staging']
 
+// TODO: we need to remove quotes from the keys
+// when we configure the eslint rules again for typescript
 export enum ECoinGeckoIdList {
-  ETH = 'ethereum'
+  'ETH' = 'ethereum'
 }
 
 export type TCoinGeckoIdKeys = keyof typeof ECoinGeckoIdList
