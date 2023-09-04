9f529aef2bc0d9252c993216557f21f79e4f9cec
idiom-bytes <idiom.bytes@gmail.com>
2023-09-04 10:43:36 -0700

    Fixing build

--- src/components/Accuracy.tsx
+++ src/components/Accuracy.tsx
@@ -3,10 +3,8 @@ import styles from '../styles/Accuracy.module.css'
 export default function Accuracy({
   accuracy
 }: {
-  accuracy: number | undefined
+  accuracy: number
 }) {
-  if (!accuracy) return 0.0
-
   return (
     <div className={styles.container}>
       <span
