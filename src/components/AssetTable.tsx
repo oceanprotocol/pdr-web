2cdcea6b007490ac7b27a10f916ece75252fe9b8
Mustafa Tuncay <mustafaislev@gmail.com>
2023-09-08 14:50:17 +0300

    the interval rate is decreased

--- src/components/AssetTable.tsx
+++ src/components/AssetTable.tsx
@@ -156,7 +156,7 @@ export const AssetTable: React.FC<TAssetTableProps> = ({ contracts }) => {
     fetchAndCacheAllPairs()
     const interval = setInterval(() => {
       fetchAndCacheAllPairs()
-    }, 60000)
+    }, 10000)
     return () => clearInterval(interval)
   }, [fetchAndCacheAllPairs])
 
