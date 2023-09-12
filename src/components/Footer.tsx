27e08f34c321ce8aa5eaf0909e62a9cf1f498683
Norbert <katunanorbert@gmail.com>
2023-09-12 15:11:27 +0200

    fixed button text color and small improvements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,21 +4,17 @@ import buttonStyles from '../styles/Button.module.css'
 export default function Footer() {
   return (
     <div className={footerStyles.container}>
-      <div>
-        <p>
-          Want to be notified when mainnet goes live? Follow us on Discord.
-        </p>
-      </div>
-      <div>
-        <a
-          href="https://discord.gg/kfrZ8wuTKc"
-          className={`${buttonStyles.button}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Discord
-        </a>
-      </div>
+      <p className={footerStyles.message}>
+        Want to be notified when mainnet goes live? Follow us on Discord.
+      </p>
+      <a
+        href="https://discord.gg/kfrZ8wuTKc"
+        className={`${buttonStyles.button}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Discord
+      </a>
     </div>
   )
 }
