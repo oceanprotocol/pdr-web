78d1669508566280d71b2581c2ed644e40fed58c
Norbert <katunanorbert@gmail.com>
2023-10-11 11:08:19 +0300

    added new page for terms

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,26 @@
 import footerStyles from '@/styles/Footer.module.css'
+import Link from 'next/link'
 import buttonStyles from '../styles/Button.module.css'
 
 export default function Footer() {
   return (
     <div className={footerStyles.container}>
-      <p className={footerStyles.message}>
-        Want to be up to date or get support? Join our Discord channel.
-      </p>
-      <a
-        href="https://discord.gg/kfrZ8wuTKc"
-        className={`${buttonStyles.button}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Discord
-      </a>
+      <div className={footerStyles.container}>
+        <p className={footerStyles.message}>
+          Want to be up to date or get support? Join our Discord channel.
+        </p>
+        <a
+          href="https://discord.gg/kfrZ8wuTKc"
+          className={`${buttonStyles.button}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Discord
+        </a>
+      </div>
+      <div className={footerStyles.container}>
+        <Link href="/terms">Terms&Conditions</Link>
+      </div>
     </div>
   )
 }
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,21 +1,14 @@
-import { AssetsContainer } from '@/components/AssetsContainer'
-import Footer from '@/components/Footer'
-
-import { TimeFrameSwitch } from '@/components/TimeFrameSwitch'
 import styles from '@/styles/Home.module.css'
 import 'react-notifications/lib/notifications.css'
 
-export default function Home() {
+export default function Terms() {
   return (
     <>
       <div className={styles.description}>
         <p className={styles.oneliner}>
           Accurate price predictions for your favorite crypto assets
         </p>
-        <TimeFrameSwitch />
       </div>
-      <AssetsContainer />
-      <Footer />
     </>
   )
 }
