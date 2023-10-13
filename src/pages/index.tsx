6e633482ce55716f13dfb0f1ab2e195eb08dd677
Mustafa Tuncay <mustafaislev@gmail.com>
2023-10-13 13:50:43 +0300

    issue-259

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import { AssetsContainer } from '@/components/AssetsContainer'
-
+import MainWrapper from '@/components/MainWrapper'
 import { TimeFrameSwitch } from '@/components/TimeFrameSwitch'
 import styles from '@/styles/Home.module.css'
+import { Inter } from 'next/font/google'
 import 'react-notifications/lib/notifications.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <MainWrapper>
       <div className={styles.description}>
         <p className={styles.oneliner}>
           Accurate price predictions for your favorite crypto assets
@@ -14,6 +17,6 @@ export default function Home() {
         <TimeFrameSwitch />
       </div>
       <AssetsContainer />
-    </>
+    </MainWrapper>
   )
 }
