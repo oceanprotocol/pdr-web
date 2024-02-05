e5f34f629d4f5d666b054e3585c502dab806af8f
Norbert <katunanorbert@gmail.com>
2024-02-05 21:04:02 +0200

    cleaned up getAccuracyData error handling

--- src/utils/getPdrBackendData.ts
+++ src/utils/getPdrBackendData.ts
@@ -3,25 +3,22 @@ import { currentConfig } from './appconstants'
 import { accuracyStatisticsEndpoint } from './endpoints/pdrBackendEndpoints'
 import { Maybe } from './utils'
 
-export const getAccuracyData = async (): Promise<Maybe<TAccuracyStatistics>> =>
-  fetch(accuracyStatisticsEndpoint(currentConfig.statisticsURL))
-    .then((response) => response.json())
-    .then((response) => {
-      // Response can return an error
-      const errorMessage = response?.message
-
-      // check if "No data available" is inside message so it can be handled correctly
-      if (
-        response?.error ||
-        errorMessage ||
-        errorMessage.includes('No data available')
-      ) {
-        console.error(errorMessage)
-        return null
-      }
-
-      return response
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+export const getAccuracyData = async (): Promise<
+  Maybe<TAccuracyStatistics>
+> => {
+  try {
+    const response = await fetch(
+      accuracyStatisticsEndpoint(currentConfig.statisticsURL)
+    )
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch accuracy data. Status: ${response.status}`
+      )
+    }
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
